06d735c95314464c4dcd7d82f49e77a0f865071e
Richard Moss <richard@englishtutorslive.com>
2017-04-26 22:27:12 +0100

    define context type on class

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux';
 
 const withAuth = ComposedComponent => {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object.isRequired
+    }
+
     render() {
+      console.log(this.context)
       return <ComposedComponent {...this.props} />;
     }
   }
