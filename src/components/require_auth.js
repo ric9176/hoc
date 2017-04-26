ede27819acd60cbb865f798af5b3642d6fbb28fc
Richard Moss <richard@englishtutorslive.com>
2017-04-26 22:14:03 +0100

    connect hoc to state

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 const withAuth = ComposedComponent => {
-  return class Authentication extends Component {
+  class Authentication extends Component {
     render() {
       return <ComposedComponent {...this.props} />;
     }
   }
+  const mapStateToProps = state => ({
+    authenticated: state.authenticated
+  })
+
+  return connect(mapStateToProps)(Authentication)
 }
 
 export default withAuth;
